6f0a18ee92c56349d605b726fabdfebbd659c26d
NoLuca <157226323+NoLuca@users.noreply.github.com>
2025-01-03 22:04:00 +0100

    Prova Menu

---
+++ Menu.java
@@ -0,0 +1,10 @@
+public class Menu {
+    private int xSquare;
+    private int ySquare;
+    private int lengthSquare;
+    private int heightSquare;
+    private int firstPointTrianglePlay;
+    private int secondPointTrianglePlay;
+    private int thirdPointTrianglePlay;
+
+}
